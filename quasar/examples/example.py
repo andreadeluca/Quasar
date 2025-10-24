aa938f18391f303a24cb29c38b3764339a71dbd9
andreadeluca <deluca.andrea29@gmail.com>
2025-10-24 14:53:17 +0200

    Code cleanup

--- quasar/examples/example.py
+++ quasar/examples/example.py
@@ -1,12 +1,15 @@
 from quasar.core.task_decorator import task
 
+
 @task()
 def first():
     print("Hi, I'm the first task!")
 
+
 @task(depends_on=["first"])
 def second():
     print("I'm the second task, after the first!")
+
 
 @task(depends_on=["second"])
 def third():
